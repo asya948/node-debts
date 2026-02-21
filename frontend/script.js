fd65c22c693e1a1505b89e79b9bb430e95bada14
Asya <ahambardzumyan2018@mail.ru>
2026-02-21 11:31:18 +0400

    i make web project file my project

--- frontend/script.js
+++ frontend/script.js
@@ -1,25 +1,25 @@
-// fetch ('http://localhost:3000/users').then( res=>res.json()).then((data)=>{
-//     console.log(data);
-// })
-// // fetch('http://localhost:3000/debts/1').then(res=>res.json()).then((data)=>{
-// //     console.log(data);
-// // })
+fetch ('http://localhost:3000/users').then( res=>res.json()).then((data)=>{
+    console.log(data);
+})
+fetch('http://localhost:3000/debts/1').then(res=>res.json()).then((data)=>{
+    console.log(data);
+})
 
-// fetch('http://localhost:3000/users/10',{
-// //     method:'DELETE',
-// //     headers:{"content-type":"application/json"},
-// // }).then(res=>res.json()).then((data)=>{
-// //     console.log(data);
-// // })
+fetch('http://localhost:3000/users/10',{
+    method:'DELETE',
+    headers:{"content-type":"application/json"},
+}).then(res=>res.json()).then((data)=>{
+    console.log(data);
+})
 
-// fetch ('http://localhost:3000/user',{
-//     method:'POST',
-//     headers:{"content-type":"application/json"},
-//     body:JSON.stringify({})
-// }).then(res=>{
-//     return res.json()
-//
-// })
+fetch ('http://localhost:3000/user',{
+    method:'POST',
+    headers:{"content-type":"application/json"},
+    body:JSON.stringify({})
+}).then(res=>{
+    return res.json()
+
+})
 
 
 
@@ -52,32 +52,37 @@ function renderUsers() {
     users.forEach(user => {
         $('#usersList').innerHTML += `
             <div class="user-item" onclick="selectUser(${user.id})">
-                ${user.name}
+                ${user.name} ${user.email}
             </div>
         `;
     });
 }
 
 function selectUser(id) {
-    let user = users.find(u => u.id === id);
+    let user = users.find(res => res.id === id);
     if (!user) return;
 
-    // թաքցնում ենք empty state
     $('#emptyState').classList.add('d-none');
-
-    // ցույց ենք տալիս user details
     $('#userDetails').classList.remove('d-none');
-
-    // լցնում ենք տվյալները
     $('#detailName').textContent = user.name;
     $('#detailEmail').textContent = user.email;
 }
 
 
+$('#orderSubmit').onclick = () => {
+   let nameEl= $('#orderName').value.trim();
+   let priceEl= $('#orderPrice').value.trim();
 
+   if (!nameEl || !priceEl) return;
 
+$('.table').innerHTML += `
+ <tr>
+                                <th style="width:55%">${nameEl}</th>
+                                <th style="width:20%">${priceEl}դրամ</th>
+                                <th style="width:25%" class="text-end">Actions}</th>
+                            </tr>`;
 
-
+}
 
 
 
